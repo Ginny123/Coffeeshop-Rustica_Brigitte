86800fafc2131a687a6f3ab3d9fc764c56e70030
brigitte <fischer-bu@t-online.de>
2021-02-28 20:11:44 +0100

    Navbar update 280221

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,12 +1,27 @@
-import { doc } from "prettier"
-
 function toggleNavbar() {
-    const toggleButton = document.querySelector(".btn-toggle");
+    const toggleButton = (document.querySelector(".btn-toggle-open") || document.querySelector(".btn-toggle-close"));
     const navbarLinks = document.querySelector(".mobnavbar");
 
     toggleButton.addEventListener("click", () => {
         navbarLinks.classList.toggle(".mobnavbar-open");
+        
     });
+    console.log("Variante: toggle");
 }
 
-export default toggleNavbar;
+// function toggleNavbar() {
+//     const toggleButton = (document.querySelector(".btn-toggle-open") || document.querySelector(".btn-toggle-close"));
+//     const navbarLinks = document.querySelector(".mobnavbar");
+
+//     toggleButton.addEventListener("click", () => {
+        
+//         if(navbarLinks.classList.contains("mobnavbar-open")) {
+//             navbarLinks.classList.remove("mobnavbar-open");
+//         } else {
+//             navbarLinks.classList.add("mobnavbar-open")
+//         }
+//     });
+//     console.log("Variante: if-else");
+// }
+
+export default toggleNavbar
